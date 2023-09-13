fbad8a19410eaf7008fad5b25cc8e3e702b0ca4f
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2023-09-13 21:35:48 +0000

    prueba 1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  document.querySelector(".card").innerHTML = randomNumber();
+  document.querySelector(".card").innerHTML = randomPintas();
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let randomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let ranNum = Math.floor(Math.random() * numbers.length);
+  return numbers[ranNum];
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let randomPintas = () => {
+  let pintas = ["♦", "♥", "♠", "♣"];
+  let ranPin = Math.floor(Math.random() * pintas.length);
+  return pintas[ranPin];
 };
