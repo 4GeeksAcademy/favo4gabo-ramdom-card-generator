26850605c07cc02fd20588c42977da52289bfa74
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2023-09-13 22:05:52 +0000

    prueba 2

--- src/app.js
+++ src/app.js
@@ -1,30 +1,36 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
 window.onload = () => {
-  document.querySelector(".card").innerHTML = randomNumber();
-  document.querySelector(".card").innerHTML = randomPintas();
-};
+  let randomNumber = () => {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let ranNum = Math.floor(Math.random() * numbers.length);
+    return numbers[ranNum];
+  };
 
-let randomNumber = () => {
-  let numbers = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let ranNum = Math.floor(Math.random() * numbers.length);
-  return numbers[ranNum];
-};
-
-let randomPintas = () => {
-  let pintas = ["♦", "♥", "♠", "♣"];
-  let ranPin = Math.floor(Math.random() * pintas.length);
-  return pintas[ranPin];
+  let randomPintas = () => {
+    let pintas = ["♦", "♥", "♠", "♣"];
+    let ranPin = Math.floor(Math.random() * pintas.length);
+    return pintas[ranPin];
+  };
+  document.querySelector(".number").innerHTML = randomNumber();
+  //   document.querySelector(".card").innerHTML = randomPintas();
 };
